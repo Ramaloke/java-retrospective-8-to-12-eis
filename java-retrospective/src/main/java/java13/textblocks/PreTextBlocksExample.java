3f5e4ac284e58df580f95633d04f019a1f3f25da
Dragon <artmtn@gmail.com>
2020-01-12 21:26:20 +0200

    Java 13: Text Blocks (preview feature) example

---
+++ java-retrospective/src/main/java/java13/textblocks/PreTextBlocksExample.java
@@ -0,0 +1,13 @@
+package java13.textblocks;
+
+public class PreTextBlocksExample {
+    public static void main(String[] args) {
+        String htmlPage ="<html>\n" +
+                "   <body>\n" +
+                "      <p>Some text here</p>\n" +
+                "   </body>\n" +
+                "</html>\n";
+
+        System.out.println(htmlPage);
+    }
+}
---
+++ java-retrospective/src/main/java/java13/textblocks/TextBlocksExample.java
@@ -0,0 +1,18 @@
+package java13.textblocks;
+
+public class TextBlocksExample {
+    public static void main(String[] args) {
+        //Java 13 preview feature
+        //Uncomment the following snippet if you use JDK 13 and preview feature is enabled
+
+        /*String htmlPage = """
+                <html>
+                    <body>
+                        <p>Some text here</p>
+                    </body>
+                </html>
+                """;
+
+        System.out.println(htmlPage);*/
+    }
+}
