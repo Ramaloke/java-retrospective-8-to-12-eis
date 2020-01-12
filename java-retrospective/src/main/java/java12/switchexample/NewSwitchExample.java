9654e4bf5b96a6b049ef464373434b3bb5e5e6aa
Dragon <artmtn@gmail.com>
2020-01-12 17:47:19 +0200

    Java 12: new experimental switch examples

---
+++ java-retrospective/src/main/java/java12/switchexample/NewSwitchExample.java
@@ -0,0 +1,46 @@
+package java12.switchexample;
+
+import java.time.Month;
+
+import static java.time.Month.*;
+
+public class NewSwitchExample {
+    public static void main(String[] args) {
+        monthsSwitchExpressionWay(JANUARY);
+
+        monthsSwitchStatementWay(JUNE);
+    }
+
+    //Java 12 expression way
+    //Uncomment method body if you use JDK 12 and experimental feature is enabled
+    private static void monthsSwitchExpressionWay(Month month) {
+        /*String result = switch(month) {
+            case DECEMBER, JANUARY, FEBRUARY -> "Winter";
+            case MARCH, APRIL, MAY -> "Spring";
+            case JUNE, JULY, AUGUST -> "Summer";
+            case SEPTEMBER, OCTOBER, NOVEMBER -> "Autumn";
+            default -> "WTF?";
+        };
+
+        System.out.println("Current season is " + result);*/
+    }
+
+    //Java 12 statement way
+    //Uncomment method body if you use JDK 12 and experimental feature is enabled
+    private static void monthsSwitchStatementWay(Month month) {
+        /*String result = switch (month) {
+            case DECEMBER, JANUARY, FEBRUARY:
+                break "Winter";
+            case MARCH, APRIL, MAY:
+                break "Spring";
+            case JUNE, JULY, AUGUST:
+                break "Summer";
+            case SEPTEMBER, OCTOBER, NOVEMBER:
+                break "Autumn";
+            default:
+                break "WTF?";
+        };
+
+        System.out.println("Current season is " + result);*/
+    }
+}
---
+++ java-retrospective/src/main/java/java12/switchexample/OldSwitchExample.java
@@ -0,0 +1,43 @@
+package java12.switchexample;
+
+import java.time.Month;
+
+import static java.time.Month.*;
+
+public class OldSwitchExample {
+
+    public static void main(String[] args) {
+        monthsSwitch(JANUARY);
+    }
+
+    private static void monthsSwitch(Month month) {
+        String result = "";
+
+        switch(month) {
+            case DECEMBER:
+            case JANUARY:
+            case FEBRUARY:
+                result = "Winter";
+                break;
+            case MARCH:
+            case APRIL:
+            case MAY:
+                result = "Spring";
+                break;
+            case JUNE:
+            case JULY:
+            case AUGUST:
+                result = "Summer";
+                break;
+            case SEPTEMBER:
+            case OCTOBER:
+            case NOVEMBER:
+                result = "Autumn";
+                break;
+            default:
+                result = "WTF?";
+        }
+
+        System.out.println("Current season is " + result);
+    }
+}
